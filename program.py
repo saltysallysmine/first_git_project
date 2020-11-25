e3d14e1f12ce7979248430f9c0a17aac5f65a141
saltysallysmine <75027326+saltysallysmine@users.noreply.github.com>
2020-11-25 17:16:52 +0300

    why...

--- program.py
+++ program.py
@@ -1,8 +1,8 @@
 def main():
     print('My first git program')
     print('And I change it every day')
-    print('Noooooooooooooooooooooo')
-    # dfigegoi
+    print('Again')
+    print('UFO came and added this line')
 
 
 if __name__ == '__main__':
